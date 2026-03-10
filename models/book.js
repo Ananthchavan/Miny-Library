fdb9c4e2638d2a415b92a0cf78311f8465dfe493
AnanthChavan <ananthchavan2006@gmail.com>
2026-03-11 00:21:19 +0530

    Defined Schema

---
+++ models/book.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    year: Number,
+    image: String,
+    price: Number,
+});
+
+const Book = mongoose.model("Book" , bookSchema);
+module.exports = Book;
