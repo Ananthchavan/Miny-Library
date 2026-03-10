b947d2d17f93d690d636b213cb540c8e54cf70b1
AnanthChavan <ananthchavan2006@gmail.com>
2026-03-11 00:22:09 +0530

    connected to the DB

--- app.js
+++ app.js
@@ -3,11 +3,33 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 
-const Book = require("./models/boook.js");
+const Book = require("./models/book.js");
 
 const MONGOURL = "mongodb://localhost:27017/library";
 
+main()
+    .then( () => {
+        console.log("connected to db");
+    })
+    .catch( (err) => {
+        console.log(err);
+    });
+
+async function main() {
+    await mongoose.connect(MONGOURL);
+}
+
+
+
 app.set("view engine" , "ejs");
-app.set("views" . path.join(__dirname,"views"));
+app.set("views" , path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
+
+app.get("/" , (req,res) => {
+    res.send("Working :)");
+});
+
+app.listen(3000 , () => {
+    console.log("Server is running on port 3000");
+});
