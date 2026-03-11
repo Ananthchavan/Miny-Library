db981ce33ae3babcd3724ae0fc62824683f5cfb1
AnanthChavan <ananthchavan2006@gmail.com>
2026-03-12 00:38:56 +0530

    Impleamented index and new routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const ejsmate = require("ejs-mate");
 
 const Book = require("./models/book.js");
 
@@ -20,14 +21,31 @@ async function main() {
 }
 
 
-
-app.set("view engine" , "ejs");
+app.engine("ejs", ejsmate);
+app.set("view engine", "ejs");
 app.set("views" , path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
 
 app.get("/" , (req,res) => {
     res.send("Working :)");
+});
+
+//All books
+app.get("/books", async (req, res) => {
+    let allBooks = await Book.find();
+    res.render("books/index", { allBooks });
+});
+
+//Add books
+app.get("/books/new" , (req,res) => {
+    res.render("books/new");
+});
+
+app.post("/books", async (req,res) => {
+    const newBook = new Book(req.body.Book);
+    await newBook.save();
+    res.redirect("/books");
 });
 
 app.listen(3000 , () => {
