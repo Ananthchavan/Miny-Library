bf90b22e7974a38bd6dfa7bd6fe457c91c96c5ec
AnanthChavan <ananthchavan2006@gmail.com>
2026-03-15 01:29:56 +0530

    Designed home page

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.engine("ejs", ejsmate);
 app.set("view engine", "ejs");
 app.set("views" , path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 
